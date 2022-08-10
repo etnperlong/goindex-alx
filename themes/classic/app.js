2072a3e21ade12af803d44cb5b3dab7aa6b3b054
Mike Evans <etnperlong@gmail.com>
2022-08-10 11:46:53 +0100

    Return datetime

--- themes/classic/app.js
+++ themes/classic/app.js
@@ -373,17 +373,8 @@ function utc2london(utc_datetime) {
     timestamp = timestamp/1000;
 
     // 时间戳转为时间 Europe/London
-    var unixtimestamp = new Date(timestamp*1000).toLocaleString('en-GB', { timeZone: 'Europe/London' });
-    var year = 1900 + unixtimestamp.getYear();
-    var month = "0" + (unixtimestamp.getMonth() + 1);
-    var date = "0" + unixtimestamp.getDate();
-    var hour = "0" + unixtimestamp.getHours();
-    var minute = "0" + unixtimestamp.getMinutes();
-    var second = "0" + unixtimestamp.getSeconds();
-    return year + "-" + month.substring(month.length-2, month.length)  + "-" + date.substring(date.length-2, date.length)
-        + " " + hour.substring(hour.length-2, hour.length) + ":"
-        + minute.substring(minute.length-2, minute.length) + ":"
-        + second.substring(second.length-2, second.length);
+    var datetime = new Date(timestamp*1000).toLocaleString('en-GB', { timeZone: 'Europe/London' });
+    return datetime;
 }
 
 // bytes自适应转换到KB,MB,GB
